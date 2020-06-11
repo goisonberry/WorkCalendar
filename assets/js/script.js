2077722d74551a3021df298f421ceb8f6d29a606
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-06-10 19:27:20 -0700

    added document.ready function and typed out what I will need in my js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 // If you can see 'Hello, World!' in the console, then you can remove this and replace it your code
 // console.log("Hello, World!");
 // YOUR JS CODE GOES HERE
+
+// Telling function to allow html and css files to run first.
+$(document).ready(function () {
+  // need function to display current date and time
+  // need function for the save buttons
+  // need to store and retrive data based on the time user saved
+  // need function to change colors based on the time of day for past, present and future
+});

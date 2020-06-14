941cebae0afff0f3143ca29f8aab79893c22da3c
goisonberry <gerson.marroquin@gmail.com>
2020-06-13 19:02:41 -0700

    all fields turned green until I changed time to military time. color change accordingly.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,15 +15,15 @@ $(document).ready(function () {
     localStorage.setItem(time, text);
   });
   // need to store and retrive data based on the time user saved
-  $("#9am .description").val(localStorage.getItem("9am"));
-  $("#10am .description").val(localStorage.getItem("10am"));
-  $("#11am .description").val(localStorage.getItem("11am"));
-  $("#12pm .description").val(localStorage.getItem("12pm"));
-  $("#1pm .description").val(localStorage.getItem("1pm"));
-  $("#2pm .description").val(localStorage.getItem("2pm"));
-  $("#3pm .description").val(localStorage.getItem("3pm"));
-  $("#4pm .description").val(localStorage.getItem("4pm"));
-  $("#5pm .description").val(localStorage.getItem("5pm"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
   // need function to change colors based on the time of day for past, present and future
   function hourTracker() {
     var currentHour = moment().hour();

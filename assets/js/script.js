d6155afb899220e7ffb6b2f327f4a692a3aa41bf
goisonberry <gerson.marroquin@gmail.com>
2020-06-13 18:26:00 -0700

    added the local storage to get information set by user.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,5 +15,14 @@ $(document).ready(function () {
     localStorage.setItem(time, text);
   });
   // need to store and retrive data based on the time user saved
+  $("#9am .description").val(localStorage.getItem("9am"));
+  $("#10am .description").val(localStorage.getItem("10am"));
+  $("#11am .description").val(localStorage.getItem("11am"));
+  $("#12pm .description").val(localStorage.getItem("12pm"));
+  $("#1pm .description").val(localStorage.getItem("1pm"));
+  $("#2pm .description").val(localStorage.getItem("2pm"));
+  $("#3pm .description").val(localStorage.getItem("3pm"));
+  $("#4pm .description").val(localStorage.getItem("4pm"));
+  $("#5pm .description").val(localStorage.getItem("5pm"));
   // need function to change colors based on the time of day for past, present and future
 });

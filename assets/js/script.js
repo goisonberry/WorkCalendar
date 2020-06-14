b52f57201d233c2be4719144b250c4f84ac42d7a
goisonberry <gerson.marroquin@gmail.com>
2020-06-13 18:17:19 -0700

    added save function for saveBtn to store user enter in formation in local sotrage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,13 @@ $(document).ready(function () {
   // need function to display current date and time
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
   // need function for the save buttons
+  $(".saveBtn").on("click", function () {
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    // Save user input in local storage.
+    localStorage.setItem(time, text);
+  });
   // need to store and retrive data based on the time user saved
   // need function to change colors based on the time of day for past, present and future
 });
